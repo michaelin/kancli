ab0e51702f5476b735654ed7304adc99ec7743f2
Michael Ingeman-Nielsen <michael@i-n.dk>
2024-03-18 09:36:21 +0100

    Add initial hardcoded task list

---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"os"
+)
+
+type status int
+
+const (
+	todo status = iota
+	inProgress
+	done
+)
+
+/* CUSTOM ITEM */
+
+type Task struct {
+	status      status
+	title       string
+	description string
+}
+
+// FilterValue implements the list.Item interface
+func (t Task) FilterValue() string {
+	return t.title
+}
+
+func (t Task) Title() string {
+	return t.title
+}
+
+func (t Task) Description() string {
+	return t.description
+}
+
+/* MAIN MODEL */
+
+type Model struct {
+	list list.Model
+	err  error
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) View() string {
+	return m.list.View()
+}
+
+func (m Model) initList(width, height int) list.Model {
+	m.list = list.New([]list.Item{}, list.NewDefaultDelegate(), width, height)
+	m.list.Title = "To Do"
+	m.list.SetItems([]list.Item{
+		Task{status: todo, title: "Buy milk", description: "Strawberry milk"},
+		Task{status: todo, title: "Eat sushi", description: "Negitoro roll, miso soup"},
+		Task{status: todo, title: "Fold laundry", description: "or wear wrinkly t-shirt"},
+	})
+	return m.list
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.list = m.initList(msg.Width, msg.Height)
+	}
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func New() *Model {
+	return &Model{}
+}
+
+func main() {
+	m := New()
+	p := tea.NewProgram(m)
+	if err := p.Start(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
